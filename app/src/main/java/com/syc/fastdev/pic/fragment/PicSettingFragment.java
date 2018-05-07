2a28c6401ebcb18b3ceadcd1273d125e808e6bb7
syc123 <990385465@qq.com>
2018-05-07 16:13:43 +0800

    ui

--- app/src/main/java/com/syc/fastdev/pic/fragment/PicSettingFragment.java
+++ app/src/main/java/com/syc/fastdev/pic/fragment/PicSettingFragment.java
@@ -7,14 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.alibaba.sdk.android.feedback.impl.FeedbackAPI;
 import com.example.basecore.base.fragment.BaseLazyFragment;
 import com.syc.fastdev.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PicSettingFragment extends BaseLazyFragment {
+public class PicSettingFragment extends BaseLazyFragment implements View.OnClickListener {
 
 
     public PicSettingFragment() {
@@ -35,7 +37,30 @@ public class PicSettingFragment extends BaseLazyFragment {
     @Override
     protected View initFragmentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pic_setting, container, false);
+        TextView setting_view = view.findViewById(R.id.setting_view);
+        TextView share_view = view.findViewById(R.id.share_view);
+        TextView feed_view = view.findViewById(R.id.feed_view);
+        TextView about_view = view.findViewById(R.id.about_view);
+        setting_view.setOnClickListener(this);
+        share_view.setOnClickListener(this);
+        feed_view.setOnClickListener(this);
+        about_view.setOnClickListener(this);
         return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.about_view:
+                break;
+            case R.id.share_view:
+                break;
+            case R.id.setting_view:
+                break;
+            case R.id.feed_view:
+                FeedbackAPI.openFeedbackActivity();
+                break;
+        }
+    }
 }
